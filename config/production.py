5bc869c2d729db0c20268a9445276f66e08f2107
Peter JH. Lee <neworld0@gmail.com>
2021-08-11 17:15:10 +0900

    config\production.py changed

--- config/production.py
+++ config/production.py
@@ -2,4 +2,4 @@ from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'webserver.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b'~\x8cP\x05\xa4\x1a\xbe\xf9%+L\xbe\x83\x80\x87\xb0'
+SECRET_KEY = b'\xf9w\xba\x02\xba\x92*\xf1\xdcS\xa1\xe8\xd6\x0e77'
