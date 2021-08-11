e6a790f5b52d5a212f453f49c8f0f14c01cfc79f
Peter JH. Lee <neworld0@gmail.com>
2021-08-11 16:48:04 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-import os
+# import os
 
-BASE_DIR = os.path.dirname(__file__)
+# BASE_DIR = os.path.dirname(__file__)
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'webserver.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'webserver.db'))
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SECRET_KEY = "dev"
