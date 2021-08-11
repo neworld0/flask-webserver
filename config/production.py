1fca0d17aad54915281dead473c902004f058fb8
neworld0 <neworld0@gmail.com>
2021-08-11 15:46:53 +0900

    웹서버 프로젝트 최초 커밋

--- config/production.py
+++
@@ -1,5 +0,0 @@
-from config.default import *
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'webserver.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b'JjK~n\xf7\x85A\xb3&F\x18\xa6f$\xc8'
