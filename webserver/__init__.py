de2f435d076129dedcd35b68b21cda08d6ceb865
Peter JH. Lee <neworld0@gmail.com>
2021-08-11 16:10:40 +0900

    init modified

--- webserver/__init__.py
+++ webserver/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flaskext.markdown import Markdown
 
+import config
+
 naming_convention = {
     "ix": 'ix_%(column_0_label)s',
     "uq": "uq_%(table_name)s_%(column_0_name)s",
