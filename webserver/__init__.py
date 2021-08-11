3a9c70a7e6656aa4d298e3c07864a9ecdf2dbc07
Peter JH. Lee <neworld0@gmail.com>
2021-08-11 18:37:12 +0900

    404 error registration

--- webserver/__init__.py
+++ webserver/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -13,6 +13,12 @@ naming_convention = {
 }
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
+
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 def create_app():
     app = Flask(__name__)
@@ -41,5 +47,9 @@ def create_app():
 
     # markdown
     Markdown(app, extensions=['nl2br', 'fenced_code'])
+
+    # 오류페이지
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, server_error)
 
     return app
