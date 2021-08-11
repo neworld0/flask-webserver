0f01be37b71841ebdc9868169f0d9022526c42fc
Peter JH. Lee <neworld0@gmail.com>
2021-08-11 21:39:13 +0900

    PostgreSQL로 변경

--- config/production.py
+++ config/production.py
@@ -1,7 +1,12 @@
 from logging.config import dictConfig
 from config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'webserver.db'))
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user='dbmasteruser',
+    pw='Y|tWr8^Po>t$:zo^y_0>ycIMot3,}0$<',
+    url='ls-bf8c8b1e79db2d507f0eda93887b0dfc674ac516.cmpkivnjfsnu.ap-northeast-2.rds.amazonaws.com',
+    db='flask_webserver')
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xf9w\xba\x02\xba\x92*\xf1\xdcS\xa1\xe8\xd6\x0e77'
 
